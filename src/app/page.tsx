2a568b1c7dd7c9d3d5132cfd676faf429c004686
SeongSeokwon <pam9411@naver.com>
2024-04-02 15:52:44 +0900

    로그인 완료시 widget 페이지로 이동 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,24 @@
 
 import Header from "@/components/Header";
 import { customAxios } from "@/libs/axios";
+import { useRouter } from "next/navigation";
+
 import { Fragment, useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
   const tokenUpdate = async () => {
+    const aToken = localStorage.getItem("UAT");
+    const rToken = localStorage.getItem("URT");
+
+    if (!(aToken && rToken)) return;
+
     try {
       const response = await customAxios
         .getInstance()
         .get("/auth/token/verify");
-      console.log(response.data);
+      router.push("widget");
     } catch (err) {
       console.log(err);
     }
