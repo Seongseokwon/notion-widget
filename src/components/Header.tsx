1fb31696155765f3cb068b25dd65c51bb61012e8
SeongSeokwon <pam9411@naver.com>
2024-04-03 11:21:32 +0900

    헤더 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { customAxios } from "@/libs/axios";
 import { useAuthStore } from "@/store/authStore";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 interface HeaderProps {}
@@ -7,9 +8,13 @@ interface HeaderProps {}
 const Header = ({}: HeaderProps) => {
   const router = useRouter();
   const { user, initUser } = useAuthStore();
-  console.log(user);
+
   const handleLogin = () => {
     router.push("/auth/login");
+  };
+
+  const handleRegister = () => {
+    router.push("/auth/register");
   };
   const handleLogout = async () => {
     await customAxios.getInstance().get("/auth/logout");
@@ -19,26 +24,81 @@ const Header = ({}: HeaderProps) => {
     router.push("/");
   };
   return (
-    <header>
-      <div>
+    <header className="flex">
+      <section className="flex-initial w-48">
+        <h2
+          className="
+        text-3xl
+        text-emerald-400
+        cursor-pointer
+        "
+          role="presentation"
+        >
+          <Link href="/">EmbedEase</Link>
+        </h2>
+      </section>
+      <section className="flex-1 flex justify-end items-center gap-5">
+        <Link className="font-semibold" href="#">
+          소개
+        </Link>
+        <Link className="font-semibold" href="#">
+          요금제
+        </Link>
         {user ? (
           <button
-            className="border border-gray-200 rounded-lg px-3 py-1"
+            className="text-white
+             border
+              bg-emerald-400
+               border-emerald-400
+                rounded-lg 
+                px-3
+                 py-2
+                 hover:bg-emerald-500
+                 hover:border-emerald-500"
             type="button"
             onClick={handleLogout}
           >
             로그아웃
           </button>
         ) : (
-          <button
-            className="border border-gray-200 rounded-lg px-3 py-1"
-            type="button"
-            onClick={handleLogin}
-          >
-            로그인
-          </button>
+          <>
+            <button
+              className="
+            text-white
+             border
+              bg-emerald-400
+               border-emerald-400
+                rounded-lg 
+                px-3
+                 py-2
+                 hover:bg-emerald-500
+                 hover:border-emerald-500
+                 "
+              type="button"
+              onClick={handleLogin}
+            >
+              로그인
+            </button>
+            <button
+              className="
+            text-white
+             border
+              bg-emerald-500
+               border-emerald-500
+                rounded-lg 
+                px-3
+                 py-2
+                 hover:bg-emerald-600
+                 hover:border-emerald-600
+                 "
+              type="button"
+              onClick={handleRegister}
+            >
+              회원가입
+            </button>
+          </>
         )}
-      </div>
+      </section>
     </header>
   );
 };
