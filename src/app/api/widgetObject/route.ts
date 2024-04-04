a5af8ec3ba0a8837aade16be7ced6886fdf0ab88
SeongSeokwon <pam9411@naver.com>
2024-04-04 12:35:43 +0900

    widget 조회 항목 수정

--- src/app/api/widgetObject/route.ts
+++ src/app/api/widgetObject/route.ts
@@ -2,7 +2,17 @@ import { NextResponse } from "next/server";
 import prisma from "@/libs/prisma";
 export async function GET() {
   try {
-    const response = await prisma.widgetObject.findMany({});
+    const response = await prisma.widgetObject.findMany({
+      select: {
+        id: true,
+        type: true,
+        name: true,
+        attributes: true,
+      },
+    });
+    const widgetObjectMap = new Map();
+
+    console.log(response);
     return NextResponse.json(response);
   } catch (err) {
     return new NextResponse("Internal Server Error", { status: 500 });
